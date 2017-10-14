fc41128538c1435db4f42611149f99cf9abff7de
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-14 11:03:47 -0400

    Progress on Plant MongoDB model.

--- api/models/Plant.js
+++ api/models/Plant.js
@@ -17,6 +17,11 @@ module.exports = {
 };
 */
 
+// This schema defines the structure for data stored in the plant collection. 
+// The plant collection stores data for both plant detail pages as well as plant category pages. 
+// This model therefore includes definitions for all categories for both types but does not require them. 
+// Simply pass in the relevant values for what you're defining.
+
 // Require mongoose
 const mongoose = require("mongoose");
 // Create Schema class
@@ -24,21 +29,29 @@ const Schema = mongoose.Schema;
 
 // Create schema
 var PlantSchema = new Schema({
-  headline: {
+  // The only required value is the title. 
+  // It should be used both for plant detail information and category information. 
+  title: {
     type: String,
     required: true, 
     unique: true
   },
-  link: {
-    type: String,
-    required: true
-  },
-  summary: {
+  // Again, it is my understanding that MongoDB stores longer text as still just strings as opposed to text blobs. 
+  // I'm not completly confident about that, however, so if we have problems I'll do more research. 
+  description: {
     type: String
+  },
+  // Bullets are stored as an array of strings.
+  bullets: {
+    type: Array
   }, 
-  byLine: {
-    type: String
+  // Enum part of this model has not been tested yet.
+  skillLevel: {
+    type: String,
+    enum: ["Easy", "Intermediate", "Difficult"]
   },
+
+  // Fields below this point are related to category objects rather than plane detail pages
 });
 
 // Create the model with the schema

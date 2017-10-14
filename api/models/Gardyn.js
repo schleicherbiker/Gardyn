5cccaca1959c54fb8e285f5dc9ac477ea1565b43
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-14 10:32:53 -0400

    Created dummy MongoDB model.

--- api/models/Gardyn.js
+++ api/models/Gardyn.js
@@ -5,10 +5,46 @@
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
+ // I'm assuming since we're using Mongoose rather than Sails' default database system this model should be replaced by a Mongoose model. 
+ // I'm leaving the original Sails model commented out in case I'm wrong about that. 
+
+/*
 module.exports = {
 
   attributes: {
 
   }
 };
+*/
 
+// Require mongoose
+const mongoose = require("mongoose");
+// Create Schema class
+const Schema = mongoose.Schema;
+
+// Create article schema
+var ArticleSchema = new Schema({
+  // title is a required string
+  headline: {
+    type: String,
+    required: true, 
+    unique: true
+  },
+  // link is a required string
+  link: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String
+  }, 
+  byLine: {
+    type: String
+  },
+});
+
+// Create the Article model with the ArticleSchema
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the model
+module.exports = Article;

6dee841ab8c479cf4941d34de4a28866b9c1014c
jeremyhe <jeremyhe1@gmail.com>
2017-10-18 19:46:28 -0400

    stuff

---
+++ client/src/components/Crop/Crop.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Crop.css";
+
+/* TODO: We need to pass in a name, a picture, and optionally a difficulty */
+
+const Crop = (props) => (    
+    <div className="crop">
+        
+    </div>
+);
+
+export default Crop;
---
+++ client/src/components/Guide/Guide.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Guide.css";
+
+/* TODO: We need to pass in a name, a picture, and optionally a difficulty */
+
+const Guide = (props) => (    
+    <div className="crop">
+    	
+    </div>
+);
+
+export default Guide;

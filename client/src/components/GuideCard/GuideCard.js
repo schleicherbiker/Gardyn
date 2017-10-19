4ef7fe5f3f11dfe03f806381376dcf695e52713f
jeremyhe <jeremyhe1@gmail.com>
2017-10-18 21:27:50 -0400

    jeremy commit

--- client/src/components/GuideCard/GuideCard.js
+++ client/src/components/GuideCard/GuideCard.js
@@ -4,9 +4,9 @@ import "./GuideCard.css";
 /* TODO: We need to pass in a name, a picture, and optionally a difficulty */
 
 const GuideCard = (props) => (    
-    <div className="guide">
-        <img className="guideImage" alt="" src="https://thumbs.dreamstime.com/z/soil-background-38546985.jpg"></img>
-        <h2 className="guideName">{props.guideName}</h2>
+    <div className="guideCard">
+        <img className="guideCardImage" alt="" src="https://thumbs.dreamstime.com/z/soil-background-38546985.jpg"></img>
+        <h2 className="guideCardName">{props.guideName}</h2>
     </div>
 );
 

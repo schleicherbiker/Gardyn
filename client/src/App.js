088a92da158392537c993856a7e7e82476aeb8fd
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-18 20:38:39 -0400

    React router implementation begun.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import AllCrops from "./components/AllCrops";
 import AllGuides from "./components/AllGuides";
 import Navbar from "./components/Navbar";
 import './App.css';
 
+// <Route path='/results' component={Results}/>
+// <Route path='/saved_articles' component={SavedArticles}/>
+
 class App extends Component {
   render() {
     return (
-      <div id="wrapper">
-        <Navbar />
-        <AllCrops />
-        <AllGuides />
-      </div>
+	    <Switch>
+	      <Route exact path='/' component={Navbar} />
+	    </Switch>
     );
   }
 }

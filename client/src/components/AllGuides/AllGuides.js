20d40389b340956532e5469ad80402728f5d65b1
Alec Schleicher <schleicher@3cisd.com>
2017-10-18 21:09:36 -0400

    Added Navbar and Wrapper to Guides and Crops route

--- client/src/components/AllGuides/AllGuides.js
+++ client/src/components/AllGuides/AllGuides.js
@@ -1,13 +1,18 @@
 import React from "react";
 import GuideCard from "../GuideCard";
+import Navbar from "../Navbar";
+import Wrapper from "../Wrapper";
 import "./AllGuides.css";
 
 const AllCrops = () => (
-    <div id="allGuides">
-        <GuideCard guideName="Soil 101"/>
-        <GuideCard guideName="Watering"/>
-        <GuideCard guideName="Pests and You"/>
-    </div>
+    <Wrapper>
+        <Navbar/>
+        <div id="allGuides">
+            <GuideCard guideName="Soil 101"/>
+            <GuideCard guideName="Watering"/>
+            <GuideCard guideName="Pests and You"/>
+        </div>
+    </Wrapper>
 );
 
 export default AllCrops;

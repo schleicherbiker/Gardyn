546b28a032a58f38317c9f287d7520346608fe3b
jeremyhe <jeremyhe1@gmail.com>
2017-10-18 20:01:43 -0400

    jeremy commit

--- client/src/components/Crop/Crop.js
+++ client/src/components/Crop/Crop.js
@@ -4,9 +4,14 @@ import "./Crop.css";
 /* TODO: We need to pass in a name, a picture, and optionally a difficulty */
 
 const Crop = (props) => (    
-    <div className="crop">
-        
+  <div>
+    <div className = "jumbotron">
+      <h2>Crop</h2>
     </div>
+    <p>
+      {/*crop info goes here*/}
+    </p>
+  </div>;
 );
 
 export default Crop;
--- client/src/components/Guide/Guide.js
+++ client/src/components/Guide/Guide.js
@@ -4,9 +4,14 @@ import "./Guide.css";
 /* TODO: We need to pass in a name, a picture, and optionally a difficulty */
 
 const Guide = (props) => (    
-    <div className="crop">
-    	
+  <div>
+    <div className = "jumbotron">
+      <h2>Guide</h2>
     </div>
+    <p>
+      {/*guide info goes here*/}
+    </p>
+  </div>;
 );
 
 export default Guide;

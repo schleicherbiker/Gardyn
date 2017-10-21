b1e94d18e89ed4c7bea3111320a49aeb220c84c9
Alec Schleicher <schleicher@3cisd.com>
2017-10-21 11:03:53 -0400

    updated styling for all crops

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,8 +3,14 @@ import "./Navbar.css";
 
 const Navbar = () => (
     <div id="navbar">
-        <img id="icon" alt="" src="./assets/leaf.png"></img>
-        <h1 id="navbarTitle">Gardyn</h1>
+        <div className="flex">
+            <img id="icon" alt="" src="./assets/leaf.png"></img>
+            <h1 id="navbarTitle">Gardyn</h1>
+        </div>
+        <div className="flex">
+            <h3 className="button"><h4>Crops</h4></h3>
+            <h3 className="button"><h4>Guides</h4></h3>
+        </div>
     </div>
 );
 

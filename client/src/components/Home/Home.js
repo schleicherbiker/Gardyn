641d93371aac1b780b0767b79ccf9022d091cb23
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-21 12:17:46 -0400

    Added navbar to homepage.

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Home.css";
+import Navbar from "../Navbar";
+import Wrapper from "../Wrapper";
 
 const Home = (props) => (
     <Wrapper>
 		<Navbar/>
-				
+		<h3>This is the homepage.</h3>		
 	</Wrapper>
 )
 

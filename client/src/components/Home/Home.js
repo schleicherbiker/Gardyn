a6d16ca26d9fdb0169fe21fac20d619bb65a58aa
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-21 12:30:35 -0400

    Displays image on homepage.

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,13 +1,26 @@
-import React from "react";
+// import React from "react";
+import React, { Component } from "react";
 import "./Home.css";
 import Navbar from "../Navbar";
 import Wrapper from "../Wrapper";
 
-const Home = (props) => (
-    <Wrapper>
-		<Navbar/>
-		<h3>This is the homepage.</h3>		
-	</Wrapper>
-)
+// const Home = (props) => (
+class Home extends Component {
+	state: {
+
+	}
+
+	farmerImageLink = "https://1yd7z7koz052nb8r33cfxyw5-wpengine.netdna-ssl.com/wp-content/uploads/2016/06/huge-old-farmer.png";
+
+	render() {
+		return (
+		    <Wrapper>
+				<Navbar/>
+				<h3>This is the homepage.</h3>
+				<img id="homeImage" src={this.farmerImageLink}/>		
+			</Wrapper>
+		)
+	}
+}
 
 export default Home;

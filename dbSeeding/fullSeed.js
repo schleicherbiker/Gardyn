ee0bef8a49545d5bb563795af0bff3101f3fc326
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-23 19:45:49 -0400

    Progress on formating seed data.

--- dbSeeding/fullSeed.js
+++ dbSeeding/fullSeed.js
@@ -46,14 +46,83 @@ db.plants.insertMany([
     "parentLevel": true,
     "parentCat": null, 
     "imageURL": "https://www.cookforyourlife.org/wp-content/uploads/2015/08/shutterstock_294838064-min.jpg"
-  }
-])
-
-
-
-// For seeding plant categories 
-db.plants.insertMany([
-    {
+  },
+  {
+        "title": 'Radishes',
+        "Climate": 'There are two main types of radishes, spring radishes and winter radishes. Avoid warm weather, radishes do not thrive above 65°F.',
+        "Sunlight": "Pick a spot that gets at least 6 hours of direct sunlight per day.",
+        "Support": 'No requirements',
+        "Spacing": 'No requirements',
+        "Water": "Water moderately. If the soil is too dry, radishes will bolt and become pithy and too pungent to eat. If too wet, the roots will split and rot. Never let the soil dry out, but don't keep it mucky, either.",
+        "Special": "Prepare a light, well-drained soil with a pH of 5.8 to 6.8 for best production. When preparing the soil, avoid fresh manure and organic materials or fertilizers high in nitrogen.",
+        "description": null,
+        "bullets": null,
+        "skillLevel": "Intermediate",
+        "parentLevel": true,
+        "parentCat": null, 
+        "imageURL": ADD 
+   },
+   {
+        "title": "Beans",
+        "Climate": "In general, beans are very sensitive to frost. Most beans grow best in air temperatures of 70°F to 80°F, and soil temperature should be at least 60°F.",
+        "Sunlight": "Beans need a sunny, well-drained area rich in organic matter.",
+        "Support": "No requirements",
+        "Spacing": "Plant most bush cultivars 3 to 6 inches apart in rows 2 to 2½ feet apart.",
+        "Water": "Water intensively once a week",
+        "Special": "Beans generally don't need extra nitrogen for good growth because the beneficial bacteria that live in nodules on bean roots help to provide nitrogen for the plants. To speed up growth, give beans—particularly long-bearing pole beans or heavy-feeding limas—a midseason side-dressing of compost or kelp extract solution.",
+        "description": null,
+        "bullets": null,
+        "skillLevel": "Intermediate",
+        "parentLevel": true,
+        "parentCat": null, 
+        "imageURL": ADD
+   },
+   {
+        "title": "Brussel Sprouts",
+        "Climate": "Growing Brussels sprouts requires cool weather. The ideal climate is the “fog belt” of the Pacific Northwest, but they will grow in just about any part of the country.",
+        "Sunlight": "Brussels sprouts need a minimum of 6 hours of sunlight daily; more is better.",
+        "Support": "No requirements",
+        "Spacing": "Brussels sprouts get large, so they need to be about 18 to 24 inches apart in a row or bed. If planted in rows, space rows 30 inches apart to give yourself enough room to walk.",
+        "Water": "Water regularly",
+        "Special": "Brussels sprouts also need more boron than most other vegetables. If your brussels sprouts develop hollow stems and small buds, you can add boron to the soil by dissolving 1 level tablespoon of borax in 5 quarts of water and sprinkling it evenly over 50 square feet of bed. DO NOT be tempted to mix more, and do not apply unless your plants have shown the deficiency symptoms we just mentioned.",
+        "description": null,
+        "bullets": null,
+        "skillLevel": "Intermediate",
+        "parentLevel": true,
+        "parentCat": null, 
+        "imageURL": ADD
+   },
+   {
+        "title": "Beets",
+        "Climate": "Beets like cool weather. In northern soils, beets shouldn’t be planted until the temperature of the soil is at least 40°F.",
+        "Sunlight": "Like most vegetables, beets prefer growing in full sun.",
+        "Support": "No requirements",
+        "Spacing": "When growing beets, plant the seeds 1 to 2 inches apart in the row.",
+        "Water": "Water regularly",
+        "Special": "You can plant beets in partial shade, but when growing beets, you want their roots to reach a depth of at least 3-6 inches, so don’t plant them under a tree where they might run into tree roots.",
+        "description": null,
+        "bullets": null,
+        "skillLevel": "Intermediate",
+        "parentLevel": true,
+        "parentCat": null, 
+        "imageURL": ADD
+   },
+   {
+        "title": "Carrots",
+        "Climate": "Start sowing this cool-weather crop 3 weeks before the last expected frost; plant again every 2 to 3 weeks after that.",
+        "Sunlight": "Carrot tops need full sun to light shade, for the carrots to grow quickly and develop their sugars.",
+        "Support": "No requirements",
+        "Spacing": "Thin to 1 inch apart when the tops are 2 inches high, and be thorough, because crowded carrots will produce crooked roots. Thin again 2 weeks later to 3 to 4 inches apart.",
+        "Water": "Water gently",
+        "Special": "To produce the best crop possible, double-dig your planting area or build up a raised bed.",
+        "description": null,
+        "bullets": null,
+        "skillLevel": "Intermediate",
+        "parentLevel": true,
+        "parentCat": null, 
+        "imageURL": ADD
+   },
+   {
         "title": ,
         "Climate": ,
         "Sunlight": ,
@@ -61,11 +130,62 @@ db.plants.insertMany([
         "Spacing": ,
         "Water": ,
         "Special": ,
-        "description": ,
-        "bullets": ,
-        "skillLevel": ,
+        "description": null,
+        "bullets": null,
+        "skillLevel": "Intermediate",
+        "parentLevel": true,
+        "parentCat": null, 
+        "imageURL": ADD
+   },
+   {
+        "title": ,
+        "Climate": ,
+        "Sunlight": ,
+        "Support": ,
+        "Spacing": ,
+        "Water": ,
+        "Special": ,
+        "description": null,
+        "bullets": null,
+        "skillLevel": "Intermediate",
         "parentLevel": BOOLEAN,
-        "parentCat": , 
-        "imageURL": 
+        "parentCat": null, 
+        "imageURL": ADD
+   },
+   {
+        "title": ,
+        "Climate": ,
+        "Sunlight": ,
+        "Support": ,
+        "Spacing": ,
+        "Water": ,
+        "Special": ,
+        "description": null,
+        "bullets": null,
+        "skillLevel": "Intermediate",
+        "parentLevel": BOOLEAN,
+        "parentCat": null, 
+        "imageURL": ADD
+   }
+])
+
+
+
+// For seeding plant categories 
+db.plants.insertMany([
+    {
+        title: 'Peas',
+        description: 'To get peas in the ground and germinating as early as possible in the spring, plant them in raised beds. The raised beds warm up faster than the surrounding ground.',
+        childPlants: [{title: 'Snap Peas', description: ADD}, {title: 'Snow Peas', description: ADD}, {title: 'Field Peas', description: ADD}]
+    },
+    {
+        title: 'Herbs',
+        description: "Recommended planting and growing these indoors. Seeds should be sowed indoors, but after 5-10 weeks seedlings will be ready to move outside. Water enough to keep the soil moist, but not soggy and avoid frequent light waterings which can draw roots to the soil surface. Certain species need extra moist soil, while others need less moist soil, so water accordingly.",
+        childPlants: [{title: 'Cilantro', description: ADD}, {title: 'Basil', description: ADD}, {title: 'Thyme', description: ADD}, {title: 'Rosemary', description: ADD}, {title: 'Mint', description: ADD}, {title: 'Chives', description: ADD}]
+    },
+    {
+        title: 'Leafy Greens',
+        description: "Edible greens. Some are great for salads, some cooking, some both.",
+        childPlants: [{title: 'Lettuce', description: ADD}, {title: 'Spinach', description: ADD}, {title: 'Kale', description: ADD}]
     }
 ])

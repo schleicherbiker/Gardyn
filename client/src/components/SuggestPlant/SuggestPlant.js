2f2a7c255f57ff45029cc11be2e65438d28ca2c7
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-24 15:59:17 -0400

    Built out SuggestPlant component further.

--- client/src/components/SuggestPlant/SuggestPlant.js
+++ client/src/components/SuggestPlant/SuggestPlant.js
@@ -10,14 +10,19 @@ class SuggestPlant extends Component {
 	}
 
 	// This function captures form data and sends it to the backend
-	captureData = () => {
+	handleSubmit = () => {
 
+	}
+
+	handleTitleChange = (e) => {
+		this.setState({title: e.target.value});
 	}
 
 	render() {
 	  return (
 			<Wrapper>
-
+				<input type="text" name="title" placeholder="Title" value={this.state.title} onChange={this.handleTitleChange} />
+				<button type="button" onClick={this.handleSubmit}>Submit</button>
 			</Wrapper>
 	  )
   }

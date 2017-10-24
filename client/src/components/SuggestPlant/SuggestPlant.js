13245f5f9b4f1ceda18dec1c12167148b0900026
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-24 16:10:31 -0400

    Attempt at nesting state.

--- client/src/components/SuggestPlant/SuggestPlant.js
+++ client/src/components/SuggestPlant/SuggestPlant.js
@@ -6,22 +6,28 @@ import axios from "axios";
 
 class SuggestPlant extends Component {
 	state = {
-
+		newPlantObj: {}
 	}
 
 	// This function captures form data and sends it to the backend
 	handleSubmit = () => {
-		console.log(this.state.title);
+		console.log(this.state.newPlantObj.title);
+		console.log(this.state.newPlantObj.climate);
 	}
 
 	handleTitleChange = (e) => {
-		this.setState({title: e.target.value});
+		this.setState({newPlantObj.title: e.target.value});
+	}
+
+	handleClimateChange = (e) => {
+		this.setState({newPlantObj.climate: e.target.value});
 	}
 
 	render() {
 	  return (
 			<Wrapper>
-				<input type="text" name="title" placeholder="Title" value={this.state.title} onChange={this.handleTitleChange} />
+				<input type="text" name="title" placeholder="Title" value={this.state.newPlantObj.title} onChange={this.handleTitleChange} />
+				<input type="text" name="climate" placeholder="Climate" value={this.state.newPlantObj.climate} onChange={this.handleClimateChange} />
 				<button type="button" onClick={this.handleSubmit}>Submit</button>
 			</Wrapper>
 	  )

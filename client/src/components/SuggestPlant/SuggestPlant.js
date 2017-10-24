f8914a0d5dbb70373bdbc567c9a8914c02afc38b
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-24 16:22:47 -0400

    Plant suggestion form built out.

--- client/src/components/SuggestPlant/SuggestPlant.js
+++ client/src/components/SuggestPlant/SuggestPlant.js
@@ -4,15 +4,17 @@ import Wrapper from "../Wrapper";
 import "./SuggestPlant.css";
 import axios from "axios";
 
+// Component that handles the suggestion of new plant entries
+// Validation should be added. 
+// Skill level should be a drop down menu rather than a text entry, if time allows, as the database will reject entries that are not just so. 
 class SuggestPlant extends Component {
 	state = {
-		
+		parentLevel: true
 	}
 
 	// This function captures form data and sends it to the backend
 	handleSubmit = () => {
-		console.log(this.state.title);
-		console.log(this.state.climate);
+		console.log(this.state);
 	}
 
 	handleChange = (e) => {
@@ -23,7 +25,13 @@ class SuggestPlant extends Component {
 	  return (
 			<Wrapper>
 				<input type="text" name="title" placeholder="Title" value={this.state.title} onChange={this.handleChange} />
-				<input type="text" name="climate" placeholder="Climate" value={this.state.climate} onChange={this.handleChange} />
+				<input type="text" name="Climate" placeholder="Climate" value={this.state.Climate} onChange={this.handleChange} />
+				<input type="text" name="Sunlight" placeholder="Sunlight" value={this.state.Sunlight} onChange={this.handleChange} />
+				<input type="text" name="Support" placeholder="Support" value={this.state.Support} onChange={this.handleChange} />
+				<input type="text" name="Spacing" placeholder="Spacing" value={this.state.Spacing} onChange={this.handleChange} />
+				<input type="text" name="Water" placeholder="Water" value={this.state.Water} onChange={this.handleChange} />
+				<input type="text" name="Special" placeholder="Special" value={this.state.Special} onChange={this.handleChange} />
+				<input type="text" name="skillLevel" placeholder="Skill level (Easy, Intermediate, Difficult)" value={this.state.skillLevel} onChange={this.handleChange} />
 				<button type="button" onClick={this.handleSubmit}>Submit</button>
 			</Wrapper>
 	  )

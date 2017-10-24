d1de8a56fffccb89a29946f1bfd989992a3d9530
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-23 21:18:17 -0400

    Fleshing out guidecard component.:

--- client/src/components/GuideCard/GuideCard.js
+++ client/src/components/GuideCard/GuideCard.js
@@ -1,13 +1,25 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import "./GuideCard.css";
 
 /* TODO: We need to pass in a name, a picture, and optionally a difficulty */
 
-const GuideCard = (props) => (    
-    <div className="guideCard">
-        <img className="guideCardImage" alt="" src="https://thumbs.dreamstime.com/z/soil-background-38546985.jpg"></img>
-        <h2 className="guideCardName">{props.guideName}</h2>
-    </div>
-);
+const GuideCard = (props) => { 
+
+	console.log("GuideCard loaded.")
+	console.log(props);
+
+	return (
+		<Link to={{
+	        pathname: `/guides/${props.item.title}`,
+	        state: {props}
+	    }}>   
+		    <div className="guideCard">
+		        <img className="guideCardImage" alt="" src="https://thumbs.dreamstime.com/z/soil-background-38546985.jpg"></img>
+		        <h2 className="guideCardName">{props.guideName}</h2>
+		    </div>
+		</Link>
+	);
+}
 
 export default GuideCard;

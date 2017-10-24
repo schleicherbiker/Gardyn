6242a9f641dbbd2b83369854ff3d0aa833dfbb6b
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-24 14:25:07 -0400

    Ternary operated added to plant detail page.

--- client/src/components/Plant/Plant.js
+++ client/src/components/Plant/Plant.js
@@ -8,38 +8,44 @@ const Plant = (props) => {
   props = props.location.state.props.item;
   console.log(props);
 
-  return (
-  	<Wrapper>
-  	  <Navbar/>
-	  <div id="individualPlantHeader">
-	  	<h2 className = "plantName">{props.title}</h2>
-	  </div>
-	  <div className = "plantAlign">
-	    <hr/>
-	    <div className = "row">
-	      <div className="col-md-3">
-	        <img className = "plantImage" src={props.imageURL} alt="200x200"/>
-	      </div>
-	  	  <div className="col-md-9">
-	  	    <p className = "plantParagraph">
-	    		Climate: {props.Climate}
-	        </p>
-	        <p className = "plantParagraph">
-	    		Sunlight: {props.Sunlight}
-	        </p>
-	        <p className = "plantParagraph">
-	    		Support: {props.Support}
-	        </p>
-	        <p className = "plantParagraph">
-	    		Spacing: {props.Spacing}
-	        </p>
-	        <p className = "plantParagraph">
-	    		Water: {props.Water}
-	        </p>
-	  	  </div>
-	    </div>
-	  </div>
-	</Wrapper>
+	  return (
+	  	<Wrapper>
+	  	  <Navbar/>
+		  <div id="individualPlantHeader">
+		  	<h2 className = "plantName">{props.title}</h2>
+		  </div>
+		  <div className = "plantAlign">
+		    <hr/>
+		    <div className = "row">
+		      <div className="col-md-3">
+		        <img className = "plantImage" src={props.imageURL} alt="200x200"/>
+		      </div>
+		      {
+		      	  (false)
+		      	  ?
+			  	  <div className="col-md-9">
+			  	    <p className = "plantParagraph">
+			    		Climate: {props.Climate}
+			        </p>
+			        <p className = "plantParagraph">
+			    		Sunlight: {props.Sunlight}
+			        </p>
+			        <p className = "plantParagraph">
+			    		Support: {props.Support}
+			        </p>
+			        <p className = "plantParagraph">
+			    		Spacing: {props.Spacing}
+			        </p>
+			        <p className = "plantParagraph">
+			    		Water: {props.Water}
+			        </p>
+			  	  </div>
+			  	  :
+			  	  <p>Not true</p>
+		  	  }
+		    </div>
+		  </div>
+		</Wrapper>
 )};
 
 export default Plant;

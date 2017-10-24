ed1d513bf57b0c723ae4546ec4247b68a6ce009e
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-24 15:49:20 -0400

    Setting up empty SuggestPlant component.

---
+++ client/src/components/SuggestPlant/SuggestPlant.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Navbar from "../Navbar";
+import Wrapper from "../Wrapper";
+import "./SuggestPlant.css";
+import axios from "axios";
+
+class SuggestPlant extends Component {
+	state = {
+
+	}
+
+	render() {
+	  return (
+			<Wrapper>
+
+			</Wrapper>
+	  )
+  }
+}
+
+export default AllPlants;

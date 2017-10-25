7c15a812bd04cf41c5984f7c7d14491f7c31cbc2
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-25 19:48:35 -0400

    Delete button now knows the ID of the target plant.

--- client/src/components/HandleSuggestions/HandleSuggestions.js
+++ client/src/components/HandleSuggestions/HandleSuggestions.js
@@ -3,6 +3,7 @@ import Navbar from "../Navbar";
 import Wrapper from "../Wrapper";
 import axios from "axios";
 import PlantCard from "../PlantCard";
+import GuideCard from "../GuideCard";
 import "./HandleSuggestions.css";
 
 class HandleSuggestions extends Component {
@@ -44,17 +45,40 @@ class HandleSuggestions extends Component {
 	    this.retrieveSuggestions();
 	}
 
+	deletePlant = (e) => {
+		// e.target.value represents the mongo ID of the submission to be deleted
+		axios.get('/api/pos_guide/' + e.target.value)
+		  .then(function (response) {
+		    console.log(response);
+			})
+		  .catch(function (error) {
+		    console.log(error);
+			});
+	};
+
 	render() {
 		return (
 		    <Wrapper>
 				<Navbar/>
+				<h2>Open Plant Suggestions</h2>
 				{
 					this.state.plantData.map(item => (
-						<PlantCard
+						<div>
+							<PlantCard
+								item = {item}
+							/>
+							<button type="button" onClick={this.deletePlant} value={item._id}>Delete</button>
+						</div>
+					))
+				}
+				<h2>Open Guide Suggestions</h2>	
+				{
+					this.state.guideData.map(item => (
+						<GuideCard
 							item = {item}
 						/>
 					))
-				}	
+				}
 			</Wrapper>
 		)
 	}

464b4a43c2c18fbf5c1ad474942b8a010b7ebb7b
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-26 13:50:22 -0400

    Guide card background image switched to something non-watermarked.

--- client/src/components/GuideCard/GuideCard.js
+++ client/src/components/GuideCard/GuideCard.js
@@ -2,6 +2,11 @@ import React from "react";
 import {Link} from "react-router-dom";
 import "./GuideCard.css";
 
+// What do you guys think about switching to a non-water marked soil image? 
+// Or even a background that's customized to the given topic (IE-- passed in through props)?
+// If not: 
+// Original background image: https://thumbs.dreamstime.com/z/soil-background-38546985.jpg
+
 const GuideCard = (props) => { 
 
 	return (
@@ -10,7 +15,7 @@ const GuideCard = (props) => {
 	        state: {props}
 	    }}>   
 		    <div className="guideCard">
-		        <img className="guideCardImage" alt="" src="https://thumbs.dreamstime.com/z/soil-background-38546985.jpg"></img>
+		        <img className="guideCardImage" alt="" src="http://earth-lite.com/wp-content/uploads/2014/07/DIRT2.jpg"></img>
 		        <h2 className="guideCardName">{props.item.title}</h2>
 		    </div>
 		</Link> 

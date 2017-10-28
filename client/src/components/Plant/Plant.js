63f5923a1629f041687a608ef809d478fc5f593f
jeremyhe <jeremyhe1@gmail.com>
2017-10-28 12:08:50 -0400

    individual plant pages css change

--- client/src/components/Plant/Plant.js
+++ client/src/components/Plant/Plant.js
@@ -43,82 +43,74 @@ const Plant = (props) => {
 		  </div>
 		  <div className = "plantAlign">
 		    <hr/>
-		    <div className = "row">
-		      <div className="col-md-6">
-		        <img className = "plantImage" src={props.imageURL} alt="200x200"/>
-		      </div>
 		      {
-		      	  (!props.isCat)
-							?
-							<div>
-							<div className="col-md-6">
+	      	  (!props.isCat)
+				?
+				<div>
+					<div className = "row plantSectionSeparate">
+				        <div className="col-md-6">
+				        	<img className = "plantImage" src={props.imageURL} alt="200x200"/>
+				        </div>
+						<div className="col-md-6">
 
-								<div className="infoDiv">
-									<img src="http://www.iconarchive.com/download/i89293/icons8/ios7/Weather-Sun.ico" id="sunlightDivIcon"></img>
-									<h4 className="textRight">{props.Sunlight} hours</h4>
-								</div>
+							<div className="infoDiv">
+								<img src="http://www.iconarchive.com/download/i89293/icons8/ios7/Weather-Sun.ico" id="sunlightDivIcon"></img>
+								<h4 className="textRight">{props.Sunlight} Hours</h4>
+							</div>
 
-								<div className="infoDiv" style={difficultyDivStyle}>
-									<h4 id="difficultyDivText">{props.skillLevel}</h4>
-								</div>
+							<div className="infoDiv" style={difficultyDivStyle}>
+								<h4 id="difficultyDivText">{props.skillLevel}</h4>
+							</div>
 
-								<div className="infoDiv">
-									<img src="http://downloadicons.net/sites/default/files/left-right-double-arrow-symbols-icons-68674.png" id="sunlightDivIcon"></img>
-									<h4 className="textRight">{props.Spacing} inches</h4>
-								</div>
+							<div className="infoDiv">
+								<img src="http://downloadicons.net/sites/default/files/left-right-double-arrow-symbols-icons-68674.png" id="sunlightDivIcon"></img>
+								<h4 className="textRight">{props.Spacing} Inches</h4>
+							</div>
 
-								<div className="infoDiv">
-									<h4 className="textLeft">Support: </h4>
-									<div className="infoDivOptions">
-										<div className="infoDivOptionsDiv" style={yesDivStyle}>
-											<img className="infoDivOptionsImage" src="http://icons.iconarchive.com/icons/icons8/android/512/Very-Basic-Checkmark-icon.png"></img>
-										</div>
-										<div id="divider"/>
-										<div className="infoDivOptionsDiv" style={maybeDivStyle}>
-											<img className="infoDivOptionsImage" src="http://icons.iconarchive.com/icons/icons8/ios7/512/Healthcare-Scales-Of-Balance-Filled-icon.png"></img>
-										</div>
-										<div id="divider"/>
-										<div className="infoDivOptionsDiv" style={noDivStyle}>
-											<img className="infoDivOptionsImage" src="https://cdn4.iconfinder.com/data/icons/geomicons/32/672366-x-128.png"></img>
-										</div>
-										
-										
+							<div className="infoDiv">
+								<h4 className="textLeft">Support: </h4>
+								<div className="infoDivOptions">
+									<div className="infoDivOptionsDiv" style={yesDivStyle}>
+										<img className="infoDivOptionsImage" src="http://icons.iconarchive.com/icons/icons8/android/512/Very-Basic-Checkmark-icon.png"></img>
+									</div>
+									<div id="divider"/>
+									<div className="infoDivOptionsDiv" style={maybeDivStyle}>
+										<img className="infoDivOptionsImage" src="http://icons.iconarchive.com/icons/icons8/ios7/512/Healthcare-Scales-Of-Balance-Filled-icon.png"></img>
+									</div>
+									<div id="divider"/>
+									<div className="infoDivOptionsDiv" style={noDivStyle}>
+										<img className="infoDivOptionsImage" src="https://cdn4.iconfinder.com/data/icons/geomicons/32/672366-x-128.png"></img>
 									</div>
 									
+									
 								</div>
+								
 							</div>
-							<p className = "plantParagraph">
-							Climate: {props.Climate}
-							</p>
-							<p className = "plantParagraph">
-							Sunlight: {props.Sunlight}
-							</p>
-							<p className = "plantParagraph">
-							Support: {props.Support}
-							</p>
-							<p className = "plantParagraph">
-							Spacing: {props.Spacing}
-							</p>
-							<p className = "plantParagraph">
-							Water: {props.Water}
-							</p>
 						</div>
-			  	  :
-			  	  <div id="catDetailsHolder">
-				  	  <p>{props.description}</p>
-				  	  <h3>Varieties:</h3>
-				  	  <div id="childPlantHolder">
-					  	  {
-							props.childPlants.map(item => (
-								<PlantCard
-									item = {item}
-								/>
-							))
-						   }
-					   </div>
-			  	  </div>
+					</div>
+					<div className = "row plantSectionSeparate">
+						<p className = "plantGuideTitle">Climate</p>
+						<p className = "plantGuideDescription">{props.Climate}</p>
+						<br />
+						<p className = "plantGuideTitle">Special</p>
+						<p className = "plantGuideDescription">{props.Special}</p>
+					</div>
+				</div>
+				:
+				<div id="catDetailsHolder">
+				  <p>{props.description}</p>
+				  <h3>Varieties:</h3>
+				  <div id="childPlantHolder">
+					{
+						props.childPlants.map(item => (
+							<PlantCard
+								item = {item}
+							/>
+						))
+					}
+				  </div>
+				</div>
 		  	  }
-		    </div>
 		  </div>
 		</Wrapper>
 )};

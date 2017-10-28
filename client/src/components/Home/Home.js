aa61293d69a3aafbd17d663ef39c3b11a38b71c7
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-28 11:39:15 -0400

    More refactoring.

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,13 +3,7 @@ import Navbar from "../Navbar";
 import Wrapper from "../Wrapper";
 import "./Home.css";
 
-// This component is not in it's final version. 
-// Just a stand in for reference purposes.
-
 class Home extends Component {
-
-	// We can make this image whatever. I just figured I'd set it up to display some image so we could see what that looked like.
-	farmerImageLink = "https://1yd7z7koz052nb8r33cfxyw5-wpengine.netdna-ssl.com/wp-content/uploads/2016/06/huge-old-farmer.png";
 
 	render() {
 		return (

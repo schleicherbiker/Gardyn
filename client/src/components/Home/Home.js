c728a2b7561f79aaf9eef2e16504e00774b42123
Alec Schleicher <schleicher@3cisd.com>
2017-10-28 11:13:47 -0400

    modified homepage

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -7,20 +7,20 @@ import "./Home.css";
 // Just a stand in for reference purposes.
 
 class Home extends Component {
-	state: {
-
-	}
 
 	// We can make this image whatever. I just figured I'd set it up to display some image so we could see what that looked like.
 	farmerImageLink = "https://1yd7z7koz052nb8r33cfxyw5-wpengine.netdna-ssl.com/wp-content/uploads/2016/06/huge-old-farmer.png";
 
 	render() {
 		return (
-		    <Wrapper>
-				<Navbar/>
-				<h3 id="homeOverlay">From planting to harvest, Gardyn helps you get into gardening faster.</h3>
-				<img id="homeImage" src={this.farmerImageLink} alt="A gardener smiles holding his freshly picked produce"/>		
-			</Wrapper>
+		    <div id="titleCenterDiv">
+				<div id="contentDiv">
+					<h1 id="homeTitle">gardyn</h1>
+					<div className="titleButton" id="allPlantsButton">Plants</div>
+					<div className="titleButton" id="allGuidesButton">Guides</div>
+					<div id="loginButton">Login</div>
+				</div>
+			</div>
 		)
 	}
 }

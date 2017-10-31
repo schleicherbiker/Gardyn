155090282edcb2331109a71c9da4be0bfe85020e
Alec Schleicher <schleicher@3cisd.com>
2017-10-30 20:57:54 -0400

    home and all plants styling

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -6,12 +6,12 @@ const Navbar = () => (
     	<a href="/" className="flat-link">
         <div className="flex">
             <img id="icon" alt="" src="../assets/leaf.png"></img>
-            <h1 id="navbarTitle">Gardyn</h1>
+            <h1 id="navbarTitle">gardyn</h1>
         </div>
       </a>
       <div className="flex">
-          <h3 className="button"><a className="nav-element-link" href="/plants"><h4>Plants</h4></a></h3>
-          <h3 className="button"><a className="nav-element-link" href="/guides"><h4>Guides</h4></a></h3>
+          <h3 className="button"><a className="nav-element-link" href="/plants"><h4>plants</h4></a></h3>
+          <h3 className="button"><a className="nav-element-link" href="/guides"><h4>guides</h4></a></h3>
       </div>
     </div>
 );
